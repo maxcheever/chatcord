1af322eb1df0574a06e5958fbd27fecb9879934f
Max Cheever <maxcheever8@hotmail.com>
2021-12-29 18:18:21 -0500

    room-specific commands

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,12 +17,21 @@ const botName = 'ChatCord Bot';
 // Run when client connects
 io.on('connection', socket => {
     socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room);
+
         // Welcome current user
         socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
         //Broadcast when a user conects
-        socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
 
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        io.emit('message', formatMessage('USER', msg));
     });
 
     // Runs when client disconnects
@@ -29,10 +39,6 @@ io.on('connection', socket => {
         io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 
-    // Listen for chatMessage
-    socket.on('chatMessage', msg => {
-        io.emit('message', formatMessage('USER', msg));
-    });
 });
 
 const PORT = 3000 || proccess.env.PORT;
